2d2f1fce6f4be180b225b0250c1951aca16c1ce5
vijay ramakrishnan <vijaykrishnan142@gmail.com>
2023-07-04 23:24:09 +0530

    js code seperated from html

---
+++ App.js
@@ -0,0 +1,15 @@
+const heading = React.createElement("div", { id: "parent" }, 
+[React.createElement("div", { id: "child 1" }, [
+    React.createElement("h1", {}, "This is heading one"),
+    React.createElement("h2", {}, "This is headinfg two")]
+),
+React.createElement("div", { id: "child 2" }, [
+    React.createElement("h1", {}, "This is heading one"),
+    React.createElement("h2", {}, "This is headinfg two")]
+)]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
